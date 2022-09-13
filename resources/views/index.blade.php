121df1c5f3350c8fa8411c2072920278aff5a53f
takumiozawa <yuusei.1107.0121@gmail.com>
2022-09-13 15:04:40 +0900

    修正3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,6 @@
   .header {
     width: 100%;
     padding: 5px;
-    background-color: #40E0D0;
     display: flex;
     justify-content: flex-start;
   }
@@ -34,28 +33,114 @@
     display: flex;
   }
   h1 {
+    width: 25vw;
     font-size: 24px;
-    margin: 5px 150px 0 0;
+    margin: 5px 0 0 0;
   }
   .header_list p {
-    margin: 10px 15px 0 150px;
+    margin: 12px 15px 0 0px;
   }
-  .logout {
-    padding: 5px 10px;
+  .logout_btn {
+    padding: 8px 10px;
     border: 2px solid red;
     border-radius: 5px;
     color: red;
     background-color: white;
-    cursor: pointer;
-    transition: .2s cubic-bezier(0.45, 0, 0.55, 1);
   }
-  .logout.reverse:hover {
+  .logout_btn:hover {
     color: white;
     background-color: red;
   }
+  .task_btn {
+    padding: 8px 10px;
+    margin-top: 10px;
+    border: 2px solid #FFD700;
+    border-radius: 5px;
+    color: #FFD700;
+    background-color: white;
+  }
+  .task_btn:hover {
+    color: white;
+    background-color: #FFD700;
+  }
+  .content {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+  }
+  .task_text {
+    width: 36vw;
+    padding: 8px 10px;
+    margin-top: 5px;
+    border: 1px solid #A9A9A9;
+    border-radius: 5px;
+  }
+  .tag_list {
+    padding: 10px;
+    border: 1px solid #A9A9A9;
+    border-radius: 5px;
+    margin-left: 25px;
+  }
+  .create_btn {
+    padding: 8px 18px;
+    border: 2px solid #FF00FF;
+    border-radius: 5px;
+    color: #FF00FF;
+    background-color: white;
+    margin-left: 25px;
+  }
+  .create_btn:hover {
+    color: white;
+    background-color: #FF00FF;
+  }
+  .title_list {
+    width: 100%;
+    margin-top: 20px;
+    font-size: 18px;
+  }
+  .list1, .list2 {
+    width: 35%;
+    text-align: center;
+  }
+  .list3, .list4, .list5 {
+    width: 10%;
+    text-align: center;
+  }
+  .text1, .text2 {
+    width: 35%;
+    text-align: center;
+  }
+  .text3, .text4, .text5 {
+    width: 10%;
+    text-align: center;
+  }
+  .update_btn {
+    padding: 8px 18px;
+    border: 2px solid #FF8C00;
+    border-radius: 5px;
+    color: #FF00FF;
+    background-color: white;
+    margin-left: 25px;
+  }
+  .update_btn:hover {
+    color: white;
+    background-color: #FF8C00;
+  }
+  .remove_btn {
+    padding: 8px 18px;
+    border: 2px solid #00FFFF;
+    border-radius: 5px;
+    color: #FF00FF;
+    background-color: white;
+    margin-left: 25px;
+  }
+  .remove_btn:hover {
+    color: white;
+    background-color: #00FFFF;
+  }
   </style>
 </head>
-  <body>
+<body>
   <div class="container">
     <div class="main">
       <div class="header">
@@ -73,12 +158,78 @@
           @endif
           <form action="/" method="get">
             @csrf
-            <input type="submit" value="ログアウト" class="logout">
+            <input type="submit" value="ログアウト" class="logout_btn">
           </form>
         </div>
       </div>
+      <form action="/find" method="get">
+        @csrf
+        <input type="submit" value="タスク検索" class="task_btn">
+      </form>
+      <div class="content" style="display:inline-flex">
+        <form action="/add" method="post">
+          @csrf
+          <input type="text" name="task" value="{{$auth->task}}" class="task_text">
+        </form>
+        <form action="detail.html" method="post">
+          @csrf
+          <select name="select" class="tag_list">
+            <option value="家事">家事</option>
+            <option value="勉強">勉強</option>
+            <option value="運動">運動</option>
+            <option value="食事">食事</option>
+            <option value="移動">移動</option>
+          </select>  
+        </form>
+        <form action="/edit" method="post">
+          @csrf
+          <input type="submit" value="追加" class="create_btn">
+        </form>  
+      </div>
+      <table class="title_list">
+        <tr>
+          <th class="list1">作成日</th>
+          <th class="list2">タスク名</th>
+          <th class="list3">タグ</th>
+          <th class="list4">更新</th>
+          <th class="list5">削除</th>
+        </tr>
+        @foreach($todos as $todo)
+        <tr>
+          <td class="text1">{{$auth->updated_at}}</td>
+          <form action="{{route('edit', ['id' => $todo->id])}}" method="post">
+            @csrf
+            <td class="text2">
+              <input type="text" name="task" value="{{$todo->task}}">
+            </td>
+          </form>
+          <form action="detail.html" method="get">
+            @csrf
+            <td class="text3">
+              <select name="select" class="tag_list">
+                <option value="家事">家事</option>
+                <option value="勉強">勉強</option>
+                <option value="運動">運動</option>
+                <option value="食事">食事</option>
+                <option value="移動">移動</option>
+              </select> 
+            </td> 
+          </form>
+          <form action="/edit" method="post">
+            @csrf
+            <td class="text4">
+              <input type="submit" value="更新" class="update_btn">
+            </td>
+          </form>  
+          <form action="/delete" method="post">
+            @csrf
+            <td class="text5">
+              <input type="submit" value="削除" class="remove_btn">
+            </td>
+          </form>  
+        </tr>
+      </table>
     </div>
   </div>
-  </body>
+</body>
 </html>
-
